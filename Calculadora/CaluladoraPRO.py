bdfbdb258b7a1177e5670c291cf262dd6370f912
silveralexojeda <156487377+silveralexojeda@users.noreply.github.com>
2024-01-30 23:22:43 -0600

    seIngresaProyCalculadora

---
+++ Calculadora/CaluladoraPRO.py
@@ -0,0 +1,5 @@
+#HDPerraEstoQuieres-EstoVasAtener
+1) Crea una calculadora que:
+•sume, reste, multiplique y divida
+•maneje los errores y valide los datos ingresados
+•tenga interfaz en la terminal, no visual
