85018fc24d7423b763fc9e9807980760b5ed1fcb
John Doe <silveralexojeda@gmail.com>
2024-01-30 23:23:52 -0600

    123

--- Calculadora/CaluladoraPRO.py
+++ Calculadora/CaluladoraPRO.py
@@ -1,5 +1,7 @@
 #HDPerraEstoQuieres-EstoVasAtener
+"""
 1) Crea una calculadora que:
 •sume, reste, multiplique y divida
 •maneje los errores y valide los datos ingresados
 •tenga interfaz en la terminal, no visual
+"""
